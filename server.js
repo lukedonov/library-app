6cb9508f2bdde0be6760f7191211ec17f700a25b
Luke Donovan <lukelamnisos@icloud.com>
2020-06-04 17:24:06 +0100

    project setup

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express');
 const app = express();
-
 const expressLayouts = require('express-ejs-layouts')
 const PORT = 3000;
+
+const indexRouter = require('./routes/index')
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true })
+
+const db = mongoose.connection
+db.on('error', error => console.log(error))
+db.once('open', () => console.log("Connected to mongoose"))
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views' );
@@ -10,8 +23,6 @@ app.set('layout', 'layouts/layouts')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.use('/', indexRouter)
 
 app.listen(process.env.PORT || PORT )
