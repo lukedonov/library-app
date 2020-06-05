6b42d40eb14e4ae834dc4681a902f060577390f2
Luke Donovan <lukelamnisos@icloud.com>
2020-06-05 14:28:49 +0100

    show all authors functionality

--- routes/authors.js
+++ routes/authors.js
@@ -3,29 +3,44 @@ const router = express.Router()
 const Author = require('../models/user')
 
 
-router.get('/', (req, res) => {
-  res.render('authors/index')
+router.get('/', async(req, res) => {
+  try {
+    const authors = await Author.find({})
+    res.render('authors/index', {authors: authors})
+  } catch {
+    res.redirect('/')
+  }
 }) 
 
 router.get('/new', (req, res) => {
   res.render('authors/new', {author: new Author() })
 })
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   const author = new Author({
     name: req.body.name
   })
-  author.save((err, newAuthor) => {
-    if (err) {
+  try {
+    const newAuthor = await author.save()
+    res.redirect('/authors')
+  } catch {
       const locals = {
         author: author,
         errorMessage: 'Error creating author'
       }
       res.render('authors/new', locals)
-    } else {
-      res.redirect('/authors')
-    }
-  }) 
+  }
+  // author.save((err, newAuthor) => {
+  //   if (err) {
+  //     const locals = {
+  //       author: author,
+  //       errorMessage: 'Error creating author'
+  //     }
+  //     res.render('authors/new', locals)
+  //   } else {
+  //     res.redirect('/authors')
+  //   }
+  // }) 
 })
 
 module.exports = router
