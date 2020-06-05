788fcda7614e208fe49005506360f1306d865402
Luke Donovan <lukelamnisos@icloud.com>
2020-06-05 12:16:27 +0100

    added form for adding new author

--- routes/authors.js
+++ routes/authors.js
@@ -1,16 +1,32 @@
 const express = require('express')
 const router = express.Router()
+const Author = require('../models/user')
+
 
 router.get('/', (req, res) => {
   res.render('authors/index')
 }) 
 
 router.get('/new', (req, res) => {
-  res.render('authors/new')
+  res.render('authors/new', {author: new Author() })
 })
 
 router.post('/', (req, res) => {
-  res.send('Create')
+  const author = new Author({
+    name: req.body.name
+  })
+
+  author.save((err, newAuthor) => {
+    if (err) {
+      res.render('/authors/new', {
+        author: author,
+        errorMessage: "Error creating Author"
+      })
+    } else {
+      // res.redirect(`/authors/${newAuthor.id}`)
+      res.redirect('/authors')
+    }
+  }) 
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const express = require('express');
+const bodyParser = require('body-parser')
+
 const app = express();
 const expressLayouts = require('express-ejs-layouts')
 const PORT = 3000;
@@ -23,6 +25,7 @@ app.set('views', __dirname + '/views' );
 app.set('layout', 'layouts/layouts')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
