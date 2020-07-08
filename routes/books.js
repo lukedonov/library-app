b7176f46f2d759fb6f7e33282194072932bcb7d7
Luke Donovan <lukelamnisos@icloud.com>
2020-07-08 13:49:27 +0100

    amended books routes

--- routes/books.js
+++ routes/books.js
@@ -21,7 +21,7 @@ router.get('/', async(req, res) => {
     query = query.regex('title', new RegExp(req.query.title, 'i'))
   }
   if (req.query.publishedBefore != null && req.query.publishedBefore != ""){
-    query = query.lte('publishDate', req.query.publishedBefore )
+   query = query.lte('publishDate', req.query.publishedBefore )
   }
   if (req.query.publishedAfter != null && req.query.publishedAfter != ""){
     query = query.gte('publishDate', req.query.publishedAfter )
